62cece204c0c25ea39832f51f41685b08b381aa6
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-11-09 18:54:41 -0300

    Changed profileIMG and Navbar

--- Client/src/views/LandingProfile/LandingProfile.jsx
+++ Client/src/views/LandingProfile/LandingProfile.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 import portfolioBackground from '../../assets/img/PortfolioBackground.png'
 import profilePicture from '../../assets/img/ProfilePicture.jpg'
+import linkedIn from '../../assets/img/linkedIn.png'
+import gitHub from '../../assets/img/gitHub.png'
 import styles from './landingProfile.module.css'
 
 const LandingProfile = () => {
@@ -9,7 +12,17 @@ const LandingProfile = () => {
             <div className={styles.divLeft}>
             </div>
             <section className={styles.sectionProfile}>
-                <img className={styles.profilePicture} src={profilePicture} alt='profileIMG'/>
+                <div className={styles.divIMG}>
+                    <img className={styles.profilePicture} src={profilePicture} alt='profileIMG'/>
+                    <div className={styles.connections}>
+                        <a href="https://www.linkedin.com/in/federicobastias/ "target="_blank" rel="noreferrer noopener">
+                            <img className={styles.connectionLinkedIn} src={linkedIn} alt='LinkedIn'/>
+                        </a>
+                        <a href='https://github.com/GabiBastias' target="_blank" rel="noreferrer noopener">
+                            <img className={styles.connectionGitHub} src={gitHub} alt='GitHub'/>
+                        </a>
+                    </div>
+                </div>
                 <h1 className={styles.h1Home}>Hi, I'm Federico Bastias, Full Stack Developer.</h1>
             </section>
         </article>
